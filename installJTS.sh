c50e49a21c00b77e6c3581e35a9f77c45ff4f2c9
sanfo3855 <sanfelicimatteo@gmail.com>
2017-08-02 10:25:16 +0200

    Pretty Install e Uninstall

--- installJTS.sh
+++ installJTS.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 if [ "$1" = "-repoGet" ]; then
+  echo "--- getting JTS from github---"
   sudo rm -r JolieTestSuite
   git clone https://github.com/sanfo3855/JolieTestSuite
+  echo "------------------------------"
+  echo ""
 fi
+sudo ./uninstallJTS.sh -noecho
+echo "installing JolieTestSuite/ to /usr/lib/"
 sudo mkdir /usr/lib/JolieTestSuite
 sudo cp -R JolieTestSuite/* /usr/lib/JolieTestSuite/
+echo "enable command line control..."
+echo "  JolieTestSuite <serviceToTest>.ol -> run the test"
 sudo cp Script/JolieTestSuite /usr/bin
 sudo chmod +x /usr/bin/JolieTestSuite
+echo "  JolieTestSuiteT <serviceToTest>.ol -> run test in trace mode"
 sudo cp Script/JolieTestSuiteT /usr/bin
 sudo chmod +x /usr/bin/JolieTestSuiteT
+echo "  JolieTestSuiteCG <serviceToTest>.ol -> genarate client"
 sudo cp Script/JolieTestSuiteCG /usr/bin
 sudo chmod +x /usr/bin/JolieTestSuiteCG
+echo "JolieTestSuite Installed!!"
+echo ""
+echo "usage:"
+echo "1. cd into service directory (there must be an init.ol file inside ./test_suite directory)"
+echo "2. JolieTestSuiteCG to generate client"
+echo "3. JolieTestSuite to run tests written inside the init.ol"
+echo ""
--- uninstallJTS.sh
+++ uninstallJTS.sh
@@ -1,4 +1,34 @@
-sudo rm -r /usr/lib/JolieTestSuite
-sudo rm /usr/bin/JolieTestSuite
-sudo rm /usr/bin/JolieTestSuiteT
-sudo rm /usr/bin/JolieTestSuiteCG
+if [ "$1" = "-noecho" ]; then
+  if [ -d "/usr/lib/JolieTestSuite" ]; then
+  sudo rm -r /usr/lib/JolieTestSuite
+  fi
+  if [ -e "/usr/bin/JolieTestSuite" ]; then
+  sudo rm /usr/bin/JolieTestSuite
+  fi
+  if [ -e "/usr/bin/JolieTestSuiteT" ]; then
+  sudo rm /usr/bin/JolieTestSuiteT
+  fi
+  if [ -e "/usr/bin/JolieTestSuiteCG" ]; then
+  sudo rm /usr/bin/JolieTestSuiteCG
+  fi
+else
+  echo "Trying to uninstall JolieTestSuite..."
+  if [ -d "/usr/lib/JolieTestSuite" ]; then
+    echo "  JolieTestSuite removed"
+  sudo rm -r /usr/lib/JolieTestSuite
+    if [ -e "/usr/bin/JolieTestSuite" ]; then
+    echo "  command line control disabled"
+    sudo rm /usr/bin/JolieTestSuite
+    fi
+    if [ -e "/usr/bin/JolieTestSuiteT" ]; then
+    sudo rm /usr/bin/JolieTestSuiteT
+    fi
+    if [ -e "/usr/bin/JolieTestSuiteCG" ]; then
+    sudo rm /usr/bin/JolieTestSuiteCG
+    fi
+    echo "Done!"
+    echo ""
+  else
+    echo "  !! JolieTestSuite not installed :/"
+  fi
+fi
