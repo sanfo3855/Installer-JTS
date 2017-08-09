5be92604e83b5fed6b33fd6faf8c02dd539d6aa2
sanfo3855 <sanfelicimatteo@gmail.com>
2017-08-09 15:25:41 +0200

    Installer done

--- installJTS.sh
+++ installJTS.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
-if [ "$1" = "-repoGet" ]; then
-  echo "--- getting JTS from github---"
-  git clone https://github.com/sanfo3855/JolieTestSuite
-  echo "------------------------------"
-  echo ""
-fi
+echo "--- getting JTS from github---"
+git clone https://github.com/sanfo3855/JolieTestSuite
+echo "------------------------------"
+echo ""
 ./uninstallJTS.sh -noecho
 echo "installing JolieTestSuite/ to /usr/lib/"
-mkdir /usr/lib/JolieTestSuite
-cp -R JolieTestSuite/* /usr/lib/JolieTestSuite/
-rm -r JolieTestSuite
+sudo mkdir /usr/lib/JolieTestSuite
+sudo cp -R JolieTestSuite/* /usr/lib/JolieTestSuite/
+sudo rm -r JolieTestSuite
 
 echo "enable command line control..."
 echo "  JolieTestSuite <serviceToTest>.ol -> run the test"
-cp Script/JolieTestSuite /usr/bin
-chmod +x /usr/bin/JolieTestSuite
+sudo cp Script/JolieTestSuite /usr/bin
+sudo chmod +x /usr/bin/JolieTestSuite
 
 echo "  JolieTestSuiteT <serviceToTest>.ol -> run test in trace mode"
-cp Script/JolieTestSuiteT /usr/bin
-chmod +x /usr/bin/JolieTestSuiteT
+sudo cp Script/JolieTestSuiteT /usr/bin
+sudo chmod +x /usr/bin/JolieTestSuiteT
 
 echo "  JolieTestSuiteCG <serviceToTest>.ol -> genarate client"
-cp Script/JolieTestSuiteCG /usr/bin
-chmod +x /usr/bin/JolieTestSuiteCG
+sudo cp Script/JolieTestSuiteCG /usr/bin
+sudo chmod +x /usr/bin/JolieTestSuiteCG
 echo "  JolieTestSuiteCGT <serviceToTest>.ol -> generate client in trace mode"
-cp Script/JolieTestSuiteCGT /usr/bin
-chmod +x /usr/bin/JolieTestSuiteCGT
+sudo cp Script/JolieTestSuiteCGT /usr/bin
+sudo chmod +x /usr/bin/JolieTestSuiteCGT
 
 echo "JolieTestSuite Installed!!"
 echo ""
--- uninstallJTS.sh
+++ uninstallJTS.sh
@@ -1,34 +1,35 @@
+#!/bin/bash
 if [ "$1" = "-noecho" ]; then
   if [ -d "/usr/lib/JolieTestSuite" ]; then
-  rm -r /usr/lib/JolieTestSuite
+  sudo rm -r /usr/lib/JolieTestSuite
   fi
   if [ -e "/usr/bin/JolieTestSuite" ]; then
-  rm /usr/bin/JolieTestSuite
+  sudo rm /usr/bin/JolieTestSuite
   fi
-  if [ -e "/usr/bin/JolieTestSuiteT"
-  rm /usr/bin/JolieTestSuiteT
+  if [ -e "/usr/bin/JolieTestSuiteT" ]; then
+  sudo rm /usr/bin/JolieTestSuiteT
   fi
   if [ -e "/usr/bin/JolieTestSuiteCG" ]; then
-  rm /usr/bin/JolieTestSuiteCG
+  sudo rm /usr/bin/JolieTestSuiteCG
   fi
   if [ -e "/usr/bin/JolieTestSuiteCGT" ]; then
-  rm /usr/bin/JolieTestSuiteCGT
+  sudo rm /usr/bin/JolieTestSuiteCGT
   fi
 else
   echo "Trying to uninstall JolieTestSuite..."
   if [ -d "/usr/lib/JolieTestSuite" ]; then
+    sudo rm -r /usr/lib/JolieTestSuite
     echo "  JolieTestSuite removed"
-  rm -r /usr/lib/JolieTestSuite
     if [ -e "/usr/bin/JolieTestSuite" ]; then
-    echo "  command line control disabled"
-    rm /usr/bin/JolieTestSuite
+    sudo rm /usr/bin/JolieTestSuite
     fi
     if [ -e "/usr/bin/JolieTestSuiteT" ]; then
-    rm /usr/bin/JolieTestSuiteT
+    sudo rm /usr/bin/JolieTestSuiteT
     fi
     if [ -e "/usr/bin/JolieTestSuiteCG" ]; then
-    rm /usr/bin/JolieTestSuiteCG
+    sudo rm /usr/bin/JolieTestSuiteCG
     fi
+    echo "  command line control disabled"
     echo "Done!"
     echo ""
   else
